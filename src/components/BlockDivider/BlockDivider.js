cacebc3b6b4179b6e4bd93eacb1b142e359c73a4
VerdiFran <tmw.kva@yandex.ru>
2021-05-04 15:48:40 +0700

    Add divider type

--- src/components/BlockDivider/BlockDivider.js
+++ src/components/BlockDivider/BlockDivider.js
@@ -6,11 +6,34 @@ import styles from './BlockDivider.module.scss'
  * @returns {JSX.Element}
  * @constructor
  */
-const BlockDivider = () => {
+const BlockDivider = ({type}) => {
+    const dividerStyles = {
+        short: {
+            start: {
+                width: '30%'
+            },
+            end: {
+                width: '70%',
+                backgroundColor: '#3268A6'
+            }
+        },
+        long: {
+            start: {
+                width: '45%'
+            },
+            end: {
+                width: '55%',
+                backgroundColor: '#002A5A'
+            }
+        }
+    }
+
+    const options = dividerStyles[type] || dividerStyles.short
+
     return (
         <div className={styles.blockDivider}>
-            <div className={styles.line}/>
-            <div className={styles.lineEnd}/>
+            <div className={styles.line} style={options.start}/>
+            <div className={styles.lineEnd} style={options.end}/>
         </div>
     )
 }
