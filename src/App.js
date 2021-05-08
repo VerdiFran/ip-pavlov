bcbaf4ed3283cf95dad870d9d2ae04932a1c8cc5
VerdiFran <tmw.kva@yandex.ru>
2021-05-08 10:17:32 +0700

    Add type attributes for BlockWrapper

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import './App.css'
-import BlockWrapper from './components/BlockWrapper/BlockWrapper'
 import PageWrapper from './components/PageWrapper/PageWrapper'
 
 function App() {
     return (
         <div className="App">
             <PageWrapper>
-                {
-                    [1, 2, 3, 4, 5].map(index => (
-                        <BlockWrapper>
-                            <div>
-                                <h1>heading {index}</h1>
-                                <h1>heading {index}</h1>
-                                <h1>heading {index}</h1>
-                                <h1>heading {index}</h1>
-                                <h1>heading {index}</h1>
-                            </div>
-                        </BlockWrapper>
-                    ))
-                }
+
             </PageWrapper>
         </div>
     )
--- src/components/BlockWrapper/BlockWrapper.js
+++ src/components/BlockWrapper/BlockWrapper.js
@@ -2,10 +2,45 @@ import React from 'react'
 import styles from './BlockWrapper.module.scss'
 import BlockDivider from './../BlockDivider/BlockDivider'
 
-const BlockWrapper = ({children}) => {
+/**
+ * Wrapper component for content block
+ * @param wrapperType
+ * @param dividerType
+ * @param hasDivider
+ * @param children
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const BlockWrapper = ({wrapperType, dividerType, children}) => {
+    const wrapperStyles = {
+        light: {
+            style: {
+                backgroundColor: '#ffffff'
+            },
+            hasDivider: true
+        },
+        dark: {
+            style: {
+                backgroundColor: '#99b4d3'
+            },
+            hasDivider: true
+        },
+        grey: {
+            style: {
+                backgroundColor: '#ececec'
+            },
+            hasDivider: false
+        }
+    }
+
+    const options = wrapperStyles[wrapperType] || wrapperStyles.light
+    const hasBlockDivider = options.hasDivider && !!dividerType
+
     return (
-        <div className={styles.blockWrapper}>
-            <BlockDivider/>
+        <div className={styles.blockWrapper} style={options.style}>
+            {
+                hasBlockDivider && <BlockDivider type={dividerType}/>
+            }
             <div className={styles.contentBlock}>{children}</div>
         </div>
     )
