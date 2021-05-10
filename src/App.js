41188edb2bd422dc8e8f61b71f06971943b284f1
VerdiFran <tmw.kva@yandex.ru>
2021-05-10 10:30:38 +0700

    Add Switch and route to home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import Header from './components/Header/Header'
-import {BrowserRouter, withRouter} from 'react-router-dom'
+import {BrowserRouter, withRouter, Switch, Route, Redirect} from 'react-router-dom'
 import './App.scss'
 import Home from './components/Home/Home'
+import * as routes from './routes'
 
 const App = () => {
     return (
         <div className="App">
             <Header/>
-            <Home/>
+            <Switch>
+                <Route
+                    exact
+                    path={routes.TO_HOME}
+                    render={() => <Home/>}
+                />
+                <Route
+                    path={routes.TO_CATALOG}
+                    render={() => <div>catalog</div>}
+                />
+                <Route
+                    path={routes.TO_ABOUT_COMPANY}
+                    render={() => <div>about company</div>}
+                />
+                <Route
+                    path={routes.TO_PARTNERS}
+                    render={() => <div>partners</div>}
+                />
+                <Route
+                    path={routes.TO_CONTACTS}
+                    render={() => <div>contacts</div>}
+                />
+                <Redirect to={routes.TO_HOME}/>
+            </Switch>
         </div>
     )
 }
