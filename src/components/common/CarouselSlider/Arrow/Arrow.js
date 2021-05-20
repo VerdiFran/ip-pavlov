eb2f96e72e417cc09681e668b846c816c0398aa8
Vladimir Rudt <vladimir.rudt@saritasa.com>
2021-05-20 21:33:03 +0700

    Add own implementation of slider

--- src/components/common/CarouselSlider/Arrow/Arrow.js
+++ src/components/common/CarouselSlider/Arrow/Arrow.js
@@ -3,15 +3,16 @@ import styles from './Arrow.module.scss'
 /**
  * Arrow for carousel slider.
  * @param type left or right.
+ * @param onClick On click action.
  */
-const Arrow = ({type}) => {
+const Arrow = ({type, onClick}) => {
     return type === 'right' ? (
-        <div className={styles.arrow}>
+        <div onClick={onClick} className={styles.arrow}>
             <div className={styles.arrowRightTop}/>
             <div className={styles.arrowRightBottom}/>
         </div>
     ) : (
-        <div className={styles.arrow}>
+        <div onClick={onClick} className={styles.arrow}>
             <div className={styles.arrowLeftTop}/>
             <div className={styles.arrowLeftBottom}/>
         </div>
--- src/components/common/CarouselSlider/CarouselSlider.js
+++ src/components/common/CarouselSlider/CarouselSlider.js
@@ -1,29 +1,101 @@
-import Carousel, { arrowsPlugin } from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
 import Arrow from './Arrow/Arrow'
+import {fadeInLeft, fadeInRight, fadeOutLeft, fadeOutRight} from 'react-animations'
+import Radium, {StyleRoot} from 'radium'
+import styles from './CarouselSlider.module.scss'
+import {useState} from 'react'
+
+const animationDuration = 0.5
+const duration = `x ${animationDuration}s`
+
+const animation = {
+
+    fadeInLeft: {
+        animation: duration,
+        animationName: Radium.keyframes(fadeInLeft, 'fadeInLeft'),
+    },
+
+    fadeOutLeft: {
+        animation: duration,
+        animationName: Radium.keyframes(fadeOutLeft, 'fadeOutLeft'),
+    },
+
+    fadeInRight: {
+        animation: duration,
+        animationName: Radium.keyframes(fadeInRight, 'fadeInRight'),
+    },
+
+    fadeOutRight: {
+        animation: duration,
+        animationName: Radium.keyframes(fadeOutRight, 'fadeOutRight'),
+    }
+}
+
 
 /**
  * Our custom carrousel slider.
  * @param props Properties
  */
 const CarouselSlider = (props) => {
+
+    const [previous, setPrevious] = useState(0)
+    const [currentAnimation, setCurrentAnimation] = useState(null)
+    const [previousAnimation, setPreviousAnimation] = useState(null)
+    const [animationStarted, setAnimationStarted] = useState(false)
+
+    const leftClickConfig = {
+        previousAnimation: animation.fadeOutRight,
+        currentAnimation: animation.fadeInLeft,
+        nextSlide: (currentSlide) =>
+            currentSlide === props.children.length - 1 ? 0 : currentSlide + 1
+    }
+
+    const rightClickConfig = {
+        previousAnimation: animation.fadeOutLeft,
+        currentAnimation: animation.fadeInRight,
+        nextSlide: (currentSlide) =>
+            currentSlide === 0 ? props.children.length - 1 : currentSlide - 1
+    }
+
+    const handleClick = (config) => {
+
+        if (animationStarted) {
+            return
+        }
+
+        setPrevious(props.current)
+        setPreviousAnimation(config.previousAnimation)
+        props.setCurrent(config.nextSlide(props.current))
+        setCurrentAnimation(config.currentAnimation)
+        setAnimationStarted(true)
+        setTimeout(() => {
+            setAnimationStarted(false)
+            setCurrentAnimation(null)
+            setPreviousAnimation(null)
+        }, animationDuration * 1000)
+    }
+
     return (
-        <Carousel
-            plugins={[
-                {
-                    resolve: arrowsPlugin,
-                    options: {
-                        arrowRight: <Arrow type={'right'}/>,
-                        arrowLeft: <Arrow type={'left'}/>,
-                        addArrowClickHandler: true,
-                    }
-                }
-            ]}
-            value={props.current}
-            onChange={slideNumber => props.setCurrent(slideNumber) }
-        >
-            {props.children}
-        </Carousel>
+        <StyleRoot>
+            <div className={styles.carousel}>
+                <div className={styles.leftArrow}>
+                    <Arrow onClick={() => handleClick(leftClickConfig)} type={'left'}/>
+                </div>
+                <div className={styles.slide}>
+                    <div className={styles.item} style={currentAnimation}>
+                        {props.children[props.current]}
+                    </div>
+                    {animationStarted &&
+                    (
+                        <div className={styles.item} style={previousAnimation}>
+                            {props.children[previous]}
+                        </div>
+                    )}
+                </div>
+                <div className={styles.rightArrow}>
+                    <Arrow onClick={() => handleClick(rightClickConfig)} type={'right'}/>
+                </div>
+            </div>
+        </StyleRoot>
     )
 }
 
