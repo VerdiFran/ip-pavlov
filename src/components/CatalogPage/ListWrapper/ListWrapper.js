2dedb5ba1cb7783edb9dddd88a1c404f90dc362d
VerdiFran <tmw.kva@yandex.ru>
2021-05-21 18:10:03 +0700

    Add wrapper for list of categories, products, etc.

---
+++ src/components/CatalogPage/ListWrapper/ListWrapper.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './ListWrapper.module.scss'
+
+/**
+ * Wrapper for list of categories or products
+ * @param title
+ * @param children
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const ListWrapper = ({title, children}) => {
+    return (
+        <div className={styles.listWrapper}>
+            <div className={styles.header}>
+                <div className={styles.title}>{title}</div>
+                <div className={styles.line}/>
+            </div>
+            <div className={styles.childrenContainer}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default ListWrapper
