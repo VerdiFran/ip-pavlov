2cca9ee497b8fffa12c3f2de0a5d7daaf53f30a0
Vladimir Rudt <vladimir.rudt@saritasa.com>
2021-05-26 09:56:24 +0700

    Make width of partner details button and partner card same

--- src/components/AboutCompany/Partners/Partner/Partner.js
+++ src/components/AboutCompany/Partners/Partner/Partner.js
@@ -1,12 +1,25 @@
 import styles from './Partner.module.scss'
+import {useResizeDetector} from 'react-resize-detector'
 
 /**
  * Component for display concrete partner.
+ * @param handleResize Invokes when partner card resizing.
  * @param name Partner name.
  */
-const Partner = ({name}) => {
+const Partner = ({handleResize, name}) => {
+    const padding = 6
+    const partnerCardPadding = {
+        padding: padding + 'px'
+    }
+
+    const {ref} = useResizeDetector({
+        onResize: (width) => {
+            handleResize && handleResize(width + padding * 2)
+        }
+    })
+
     return (
-        <div className={styles.partnerCard}>
+        <div ref={ref} style={partnerCardPadding} className={styles.partnerCard}>
             <span className={styles.partnerName}>{name}</span>
         </div>
     )
--- src/components/AboutCompany/Partners/Partners.js
+++ src/components/AboutCompany/Partners/Partners.js
@@ -1,24 +1,37 @@
 import styles from './Partners.module.scss'
 import Partner from './Partner/Partner'
 import {NavLink} from 'react-router-dom'
+import React, {useState} from 'react'
+
+/**
+ * Initial style of partners details button.
+ */
+const initialButtonStyle = {
+    width: "200px"
+}
 
 /**
  * Component for display list of card with partners names.
  * @param partners List of partners.
  */
 const Partners = ({partners}) => {
+    const [buttonStyle, setButtonStyle] = useState(initialButtonStyle)
+
+    const handleResize = (width) => setButtonStyle({
+        width: width
+    })
 
     return (
         <div className={styles.partnersBlock}>
             <h1 className="heading">наши партнеры</h1>
             <div className={styles.partnersContainer}>
                 {
-                    partners.map(partner =>
-                        <Partner name={partner.name}/>
+                    partners.map((partner, idx) =>
+                        <Partner handleResize={idx === 0 && handleResize} name={partner.name}/>
                     )
                 }
             </div>
-            <NavLink className={styles.anotherPartnersButton} to="/partners">подробнее о партнерах</NavLink>
+            <NavLink style={buttonStyle} className={styles.partnersDetailsButton} to="/partners">подробнее о партнерах</NavLink>
         </div>
     )
 }
