16768d2de039c638c25cb5777256c76dffa75586
Vladimir Rudt <vladimir.rudt@saritasa.com>
2021-05-26 23:16:10 +0700

    Add mapContainerId to Map, add initial Representatives component

---
+++ src/components/Contacts/Representatives/Representatives.js
@@ -0,0 +1,9 @@
+import Map from '../../common/Map/Map'
+
+const Representatives = ({representatives}) => {
+    return <div>
+        <Map mapContainerId={'representatives-map'}/>
+    </div>
+}
+
+export default Representatives
