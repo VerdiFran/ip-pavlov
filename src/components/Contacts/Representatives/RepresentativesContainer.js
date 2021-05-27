7d4ab1625daa5815dfd7a7bc2521d9531d66e57a
Vladimir Rudt <vladimir.rudt@saritasa.com>
2021-05-27 22:11:10 +0700

    Add sales representatives uploading, modify Map component

--- src/components/Contacts/Representatives/RepresentativesContainer.js
+++ src/components/Contacts/Representatives/RepresentativesContainer.js
@@ -1,7 +1,18 @@
 import Representatives from './Representatives'
+import {useEffect, useState} from 'react'
+import {representativesApi} from '../../../api/representativesApi'
 
 const RepresentativesContainer = () => {
-    return <Representatives representatives={[]}/>
+    const [representatives, setRepresentatives] = useState([])
+
+    useEffect(() => {
+        representativesApi.getSalesRepresentatives()
+            .then((response) => {
+                setRepresentatives(response.data)
+            })
+    }, [])
+
+    return <Representatives representatives={representatives}/>
 }
 
 export default RepresentativesContainer
--- src/components/common/Map/Map.js
+++ src/components/common/Map/Map.js
@@ -5,28 +5,30 @@ import DG from '2gis-maps'
 /**
  * Component with map.
  */
-const Map = ({polygons ,mapContainerId}) => {
+const Map = ({center, zoom, polygons ,mapContainerId}) => {
+
     useEffect(() => {
         const map = DG.map(mapContainerId, {
-            'center': [56.0905, 92.999355],
-            'zoom': 11
+            'center': center ?? [56.010052, 92.852600],
+            'zoom': zoom ?? 11
         })
 
         if (polygons) {
             DG.then(() => {
                 polygons.forEach(poly => {
-                    const polygon = DG.polygon(poly.coordinates, poly.description.style).addTo(map)
-                    const polygonBounds = polygon.getBounds()
+                    if (!poly.coordinates) {
+                        return
+                    }
+
+                    const polygon = DG.polygon(poly.coordinates, poly?.description?.style ?? {}).addTo(map)
 
                     polygon.on('mouseover', () => {
-                        poly?.onMouseOver(poly)
+                        poly?.onMouseOver && poly.onMouseOver(poly)
                     })
 
                     polygon.on('mouseout', () => {
-                        poly?.onMouseOut(poly)
+                        poly?.onMouseOut && poly.onMouseOut(poly)
                     })
-
-                    map.fitBounds(polygonBounds);
                 })
             })
         }
