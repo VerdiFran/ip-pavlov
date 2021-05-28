b3a9da14ea1e2fcc3e74cc2a81de130ce774eb11
Vladimir Rudt <vladimir.rudt@saritasa.com>
2021-05-28 14:35:18 +0700

    Add copy email to clipboard

--- src/components/Contacts/OurContacts/OurContacts.js
+++ src/components/Contacts/OurContacts/OurContacts.js
@@ -2,8 +2,33 @@ import styles from './OurContacts.module.scss'
 import call from '../../../assets/images/contacts/call.svg'
 import email from '../../../assets/images/contacts/open-email.svg'
 import address from '../../../assets/images/contacts/address.svg'
+import {useState} from 'react'
 
 const OurContacts = () => {
+    const defaultCopyButtonStyle = {
+        border: '2px solid #3268A6'
+    }
+
+    const activeCopyButtonStyle = {
+        border: '2px solid #4BB543'
+    }
+
+    const [copyButtonStyle, setCopyButtonStyle] = useState(defaultCopyButtonStyle)
+    const [tip, setTip] = useState('Скопировать')
+
+    const emailContact = 'ip.pavlov@example.com'
+    const handleCopy = () => {
+        setCopyButtonStyle(activeCopyButtonStyle)
+        navigator.clipboard.writeText(emailContact)
+            .then(() => {
+                setTip('Скопировано!')
+                setTimeout(() => {
+                    setCopyButtonStyle(defaultCopyButtonStyle)
+                    setTip('Скопировать')
+                }, 3000)
+            })
+    }
+
     return (
         <div className={styles.contactsSection}>
             <h1 className="heading">наши контакты</h1>
@@ -17,10 +42,19 @@ const OurContacts = () => {
                 </div>
                 <div className={styles.contactContainer}>
                     <div className={styles.contactTitle}>
-                        <img src={email} alt={email} className={styles.contactImage}/>
+                        <img src={email} alt="email" className={styles.contactImage}/>
                         <span>email</span>
                     </div>
-                    <span className={styles.contact}>ip.pavlov@example.com</span>
+                    <div className={styles.contactWithCopy}>
+                        <span className={styles.contact}>{emailContact}</span>
+                        <div onClick={handleCopy} className={styles.copy}>
+                            <div className={styles.copyButton}>
+                                <div style={copyButtonStyle} className={styles.backCopy}/>
+                                <div style={copyButtonStyle} className={styles.frontCopy}/>
+                            </div>
+                            <div className={styles.tip}>{tip}</div>
+                        </div>
+                    </div>
                 </div>
                 <div className={styles.contactContainer}>
                     <div className={styles.contactTitle}>
