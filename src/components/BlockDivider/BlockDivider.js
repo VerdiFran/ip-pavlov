41353966554fec49c16a3355a0cbe194a572feda
VerdiFran <tmw.kva@yandex.ru>
2021-05-28 16:43:49 +0700

    Animate divider lines

--- src/components/BlockDivider/BlockDivider.js
+++ src/components/BlockDivider/BlockDivider.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import styles from './BlockDivider.module.scss'
+import useDebounce from '../../hooks/useDebounce'
 
 /**
  * Styled divider for dividing content blocks
@@ -7,23 +8,46 @@ import styles from './BlockDivider.module.scss'
  * @constructor
  */
 const BlockDivider = ({type}) => {
+    const startRef = useRef()
+    const endRef = useRef()
+
+    const [shortEndWidth, setShortEndWidth] = useState(10)
+    const [longEndWidth, setLongEndWidth] = useState(55)
+
+    const debouncedShortEndWidth = useDebounce(shortEndWidth, 10)
+    const debouncedLongEndWidth = useDebounce(longEndWidth, 10)
+
+    const handleScroll = () => {
+        const winScroll = document.body.scrollTop || document.documentElement.scrollTop
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        const scrolled = (winScroll / height) * 50
+        setShortEndWidth(scrolled + 20)
+        setLongEndWidth(100 - scrolled - 20)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
     const dividerStyles = {
         short: {
             start: {
-                width: '30%'
+                width: `${100 - debouncedShortEndWidth}%`,
+                transitionDuration: '0.9s'
             },
             end: {
-                width: '70%',
-                backgroundColor: '#3268A6'
+                width: `${debouncedShortEndWidth}%`,
+                backgroundColor: '#3268A6',
+                transitionDuration: '0.9s'
             }
         },
         long: {
             start: {
-                width: '45%'
+                width: `${100 - debouncedLongEndWidth}%`,
+                transitionDuration: '0.9s'
             },
             end: {
-                width: '55%',
-                backgroundColor: '#002A5A'
+                width: `${debouncedLongEndWidth}%`,
+                backgroundColor: '#002A5A',
+                transitionDuration: '0.9s'
             }
         }
     }
@@ -32,8 +56,8 @@ const BlockDivider = ({type}) => {
 
     return (
         <div className={styles.blockDivider}>
-            <div className={styles.line} style={options.start}/>
-            <div className={styles.lineEnd} style={options.end}/>
+            <div className={styles.line} style={options.start} ref={startRef}/>
+            <div className={styles.lineEnd} style={options.end} ref={endRef}/>
         </div>
     )
 }
