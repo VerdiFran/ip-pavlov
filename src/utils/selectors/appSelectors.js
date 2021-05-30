96eec6d97d25af96f48de6cb2496db2a9b31feec
VerdiFran <tmw.kva@yandex.ru>
2021-05-30 13:09:09 +0700

    Add success and error messages after submitting

--- src/utils/selectors/appSelectors.js
+++ src/utils/selectors/appSelectors.js
@@ -1,1 +1,7 @@
 export const getInitialized = (state) => state.app.initialized
+
+export const getMessageInfo = (state) => ({
+    status: state.app.messageStatus,
+    text: state.app.message,
+    visible: state.app.messageVisible
+})
