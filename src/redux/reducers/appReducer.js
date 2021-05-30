b3ef983f13b86b058987b6bf7077273b7b707b71
VerdiFran <tmw.kva@yandex.ru>
2021-05-30 13:09:09 +0700

    Add success and error messages after submitting

--- src/redux/reducers/appReducer.js
+++ src/redux/reducers/appReducer.js
@@ -1,10 +1,32 @@
 import {downloadPartners} from './catalogReducer'
 import {chooseEightRandomCategories} from './catalogReducer'
 
-const INITIALIZED_SUCCESS = 'INITIALIZED_SUCCESS'
+const INITIALIZED_SUCCESS = 'APP/INITIALIZED_SUCCESS'
+const SET_MESSAGE = 'APP/SET-MESSAGE'
+const DELETE_MESSAGE = 'APP/DELETE-MESSAGE'
+
+export const messageStatuses = {
+    success: 'SUCCESS',
+    error: 'ERROR'
+}
+
+const messagesClassnames = {
+    [messageStatuses.success]: 'message success',
+    [messageStatuses.error]: 'message error'
+}
+
+/**
+ * Get classnames for message
+ * @param status Status of message
+ * @returns {*}
+ */
+export const getMessageClassname = (status) => messagesClassnames[status]
 
 const initialState = {
-    initialized: false
+    initialized: false,
+    message: '',
+    messageVisible: false,
+    messageStatus: null
 }
 
 const appReducer = (state = initialState, action) => {
@@ -15,6 +37,20 @@ const appReducer = (state = initialState, action) => {
                 initialized: true
             }
         }
+        case SET_MESSAGE:
+            return {
+                ...state,
+                message: action.message,
+                messageVisible: true,
+                messageStatus: action.status
+            }
+        case DELETE_MESSAGE:
+            return {
+                ...state,
+                messageVisible: false,
+                message: '',
+                messageStatus: null
+            }
         default: {
             return state
         }
@@ -22,6 +58,8 @@ const appReducer = (state = initialState, action) => {
 }
 
 const initializedSuccess = () => ({type: INITIALIZED_SUCCESS})
+const setMessage = (message, status) => ({type: SET_MESSAGE, message, status})
+const deleteMessage = () => ({type: DELETE_MESSAGE})
 
 export const initializeApp = () => async (dispatch) => {
     const promises = []
@@ -33,6 +71,13 @@ export const initializeApp = () => async (dispatch) => {
         })
 
     dispatch(await downloadPartners())
+}
+
+export const showMessage = (status, message) => (dispatch) => {
+    dispatch(setMessage(message, status))
+    setTimeout(() => {
+        dispatch(deleteMessage())
+    }, 4000)
 }
 
 export default appReducer
