3e0539c22f99b54fa3cc0d72604a87301fe4b2a6
Vladimir Rudt <vladimir.rudt@saritasa.com>
2022-08-15 21:05:22 +0700

    Hide promotions if it is empty

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,40 @@
 import React, {useEffect} from 'react'
+import {compose} from 'redux'
+import {connect} from 'react-redux'
 import BlockWrapper from '../BlockWrapper/BlockWrapper'
 import AboutSite from './AboutSite/AboutSite'
 import PageWrapper from '../PageWrapper/PageWrapper'
 import CatalogSectionContainer from './CatalogSection/CatalogSectionContainer'
 import PromotionsContainer from './Promotions/PromotionsContainer'
+import {getPromotions} from '../../utils/selectors/PromotionsSelectors'
+
+const mapStateToProps = (state) => ({
+    promotions: getPromotions(state)
+})
 
 /**
  * Home page
  * @returns {JSX.Element}
  * @constructor
  */
-const Home = () => {
+const Home = ({promotions}) => {
     useEffect(() => {
         document.title = 'Главная · ИП Павлов'
     }, [])
+
+    const promotionsSection = !promotions
+        ? (<></>)
+        : (
+        <BlockWrapper wrapperType="light" dividerType="short">
+            <PromotionsContainer/>
+        </BlockWrapper>
+        )
 
     return (
         <>
             <AboutSite/>
             <PageWrapper>
-                <BlockWrapper wrapperType="light" dividerType="short">
-                    <PromotionsContainer/>
-                </BlockWrapper>
+                {promotionsSection}
                 <BlockWrapper wrapperType="dark" dividerType="long">
                     <CatalogSectionContainer/>
                 </BlockWrapper>
@@ -30,4 +43,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default compose(connect(mapStateToProps))(Home)
