ca362fd30eeccc85fe8afd38cb23d4ba6dc4c2a2
Vladimir Rudt <vladimir.rudt@saritasa.com>
2022-08-26 22:14:05 +0700

    fix error/success messages on form sending

--- src/components/CatalogPage/CooperationForm/CooperationFormContainer.js
+++ src/components/CatalogPage/CooperationForm/CooperationFormContainer.js
@@ -1,16 +1,29 @@
 import CooperationForm from './CooperationForm'
 import cooperationRequestsApi from '../../../api/cooperationRequestsApi'
+import {connect, useDispatch} from 'react-redux'
+import {messageStatuses, showMessage} from '../../../redux/reducers/appReducer'
 
 const CooperationFormContainer = ({objRef, close}) => {
-    const handleSubmit = (cooperationRequest) => {
-        cooperationRequestsApi.sendCooperationRequest({
-            ...cooperationRequest,
-            company: cooperationRequest.companyName,
-            phone: cooperationRequest.phoneNumber
-        })
+    const dispatch = useDispatch()
+
+    const handleSubmit = async (cooperationRequest) => {
+        try {
+            const response = await cooperationRequestsApi.sendCooperationRequest({
+                ...cooperationRequest,
+                company: cooperationRequest.companyName,
+                phone: cooperationRequest.phoneNumber
+            })
+
+            if (response.status === 200) {
+                dispatch(showMessage(messageStatuses.success, 'Ваш запрос успешно отправлен!'))
+                close()
+            }
+        } catch {
+            dispatch(showMessage(messageStatuses.error, 'Что-то пошло не так, попробуйте позже.'))
+        }
     }
 
     return <CooperationForm handleSubmit={handleSubmit} objRef={objRef} close={close}/>
 }
 
-export default CooperationFormContainer
+export default connect(null, {showMessage})(CooperationFormContainer)
